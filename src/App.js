3d2b1c9eced55ba413e2854eba19aeff19fcb58d
Rune Niska <niska.rune@gmail.com>
2022-01-21 11:02:16 +0200

    Init bloglist frontend and enable logging in (5.1)

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs => {
+      setBlogs(blogs)
+    })
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('wrong credentials')
+    }
+  }
+
+  const handleUsernameChange = ({ target }) => (
+    setUsername(target.value)
+  )
+
+  const handlePasswordChange = ({ target }) => {
+    setPassword(target.value)
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <LoginForm 
+          handleSubmit={handleLogin}
+          handleUsernameChange={handleUsernameChange}
+          handlePasswordChange={handlePasswordChange}
+          username={username}
+          password={password}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>Blogs</h2>
+      <p>{user.name} logged in</p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ src/services/blogs.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
+
+const exports = { getAll }
+
+export default exports
