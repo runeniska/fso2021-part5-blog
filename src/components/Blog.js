578e02d8da00ed269df5faea8eef0b50e94d1e68
Rune Niska <niska.rune@gmail.com>
2022-01-27 09:28:40 +0200

    Extend blog information when a blog is clicked (5.7)

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,25 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false)
+
+  if (showAll) {
+    return (
+      <div className='blog'>
+        {blog.title} {blog.author}
+        <button onClick={() => setShowAll(false)}>hide</button>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes} <button>like</button></div>
+        <div>{blog.user.name}</div>
+      </div>
+    )
+  }
+
+  return (
+    <div className='blog'>
+      {blog.title} {blog.author}
+      <button onClick={() => setShowAll(true)}>view</button>
+    </div>
+  )
+}
 
 export default Blog
